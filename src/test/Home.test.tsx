747bb717b043b8be5aefbbf511001917bee64dcd
Nils Abels <nils.abels23@gmail.com>
2022-12-21 11:52:47 +0100

    Navbar(2#)

--- src/test/Home.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Home from "../pages/Home/Home";
-
-test('renders learn react link', () => {
-    render(<Home />);
-    const linkElement = screen.getByText(/Veranstaltungen/i);
-    expect(linkElement).toBeInTheDocument();
-});
