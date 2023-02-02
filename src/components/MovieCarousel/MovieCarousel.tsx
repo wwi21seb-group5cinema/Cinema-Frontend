3bd255c3f70373644d54c017afcdc29922591f11
pbahde <bahde.paul@gmail.com>
2023-02-02 17:11:39 +0100

    movie carousel

--- src/components/MovieCarousel/MovieCarousel.tsx
+++ src/components/MovieCarousel/MovieCarousel.tsx
@@ -1,34 +1,13 @@
 import React from 'react';
 import { Carousel } from 'antd';
-
-const contentStyle: React.CSSProperties = {
-    height: '160px',
-    color: '#fff',
-    lineHeight: '160px',
-    textAlign: 'center',
-    background: '#364d79',
-};
-
 interface Props {
-    imageUrl: string;
-    title: string;
+    Movies: React.ReactElement[]
 }
 
-const MovieCarousel: React.FC<Props> = ({ imageUrl, title }) => (
+const MovieCarousel: React.FC<Props> = ({ Movies }) => (
 
     <Carousel autoplay>
-        <div>
-            <h3 style={contentStyle}>1</h3>
-        </div>
-        <div>
-            <h3 style={contentStyle}>2</h3>
-        </div>
-        <div>
-            <h3 style={contentStyle}>3</h3>
-        </div>
-        <div>
-            <h3 style={contentStyle}>4</h3>
-        </div>
+        {Movies}
     </Carousel>
 
 );
