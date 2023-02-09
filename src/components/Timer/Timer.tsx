6eca26b014937d04de18740484de38a547470423
Nico Niebisch <95490170+Nico1808@users.noreply.github.com>
2023-02-09 18:08:57 +0100

    Add Timer for Booking Workflow (#23)
    Co-authored-by: Niebisch <nico.niebisch@sap.com>
    Co-authored-by: pbahde <bahde.paul@gmail.com>
    Co-authored-by: pbahde <95094589+pbahde@users.noreply.github.com>
    Co-authored-by: abramsmax <92868348+abramsmax@users.noreply.github.com>

---
+++ src/components/Timer/Timer.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import {useNavigate} from "react-router-dom";
+
+interface Props {
+    deadline: string;
+    display: boolean;
+}
+
+const Timer = ({deadline, display}: Props) => {
+
+    const [minutes, setMinutes] = useState(-1);
+    const [seconds, setSeconds] = useState(0);
+    const navigate = useNavigate();
+    const contentStyle: React.CSSProperties = {
+        fontSize : "1.5rem",
+        marginTop : "5px"
+    };
+
+    const getTime = (deadline: string) => {
+        const time = Date.parse(deadline) - Date.now();
+
+        setMinutes(Math.floor((time / 1000 / 60) % 60));
+        setSeconds(Math.floor((time / 1000) % 60));
+        if(time ===0 && display)
+        {
+            alert("Die Reservierung ist abgelaufen! Du wirst auf die Startseite zurückgeführt!");
+            navigate('../');
+        }
+    };
+
+    useEffect(() => {
+        const interval = setInterval(() => getTime(deadline), 1000);
+
+        return () => clearInterval(interval);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    function getNumberDisplay(num:number)
+    {
+        return num<10 ? "0"+num : num;
+    }
+    return (<>
+        {((minutes >-1) && display) &&
+            <div className="timer" style={contentStyle}>
+                Tickets reserviert für: {getNumberDisplay(minutes)}:{getNumberDisplay(seconds)} Minuten
+        </div>}
+        </>
+    );
+};
+
+export default Timer;
